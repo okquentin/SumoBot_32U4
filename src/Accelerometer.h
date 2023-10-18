3a0e6e4947ed18733a2ec79e9c148f70ca8550fa
okquentin <qosterhage@outlook.com>
2023-10-17 20:40:00 -0400

    Used the collision detection example, migrated classes to separate files

---
+++ src/Accelerometer.h
@@ -0,0 +1,36 @@
+// Accelerometer Class -- extends the Zumo32U4IMU class to support reading and
+//   averaging the x-y acceleration vectors from the accelerometer
+#include <RunningAverage.h>
+#include <Zumo32U4.h>
+#include <Arduino.h>
+
+
+// Accelerometer Settings
+#define RA_SIZE 3  // number of readings to include in running average of accelerometer readings
+#define XY_ACCELERATION_THRESHOLD 2400  // for detection of contact (~16000 = magnitude of acceleration due to gravity)
+class Accelerometer : public Zumo32U4IMU
+{
+  typedef struct acc_data_xy
+  {
+    unsigned long timestamp;
+    int x;
+    int y;
+    float dir;
+  } acc_data_xy;
+
+  public:
+    Accelerometer() : ra_x(RA_SIZE), ra_y(RA_SIZE) {};
+    ~Accelerometer() {};
+    void getLogHeader(void);
+    void readAcceleration(unsigned long timestamp);
+    float len_xy() const;
+    float dir_xy() const;
+    int x_avg(void) const;
+    int y_avg(void) const;
+    long ss_xy_avg(void) const;
+    float dir_xy_avg(void) const;
+  private:
+    acc_data_xy last;
+    RunningAverage<int> ra_x;
+    RunningAverage<int> ra_y;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,157 +1,37 @@
-/* This example uses the Zumo 32U4's onboard accelerometer to detect contact with an adversary robot
- * in the sumo ring.
- *
- * This example extends the BorderDetect example, which makes use of the line sensors on Zumo 32U4
- * Front Sensor Array to detect the border of the sumo ring.  It also illustrates the use of the
- * motors, pushbuttons, display, and buzzer.
- *
- * In loop(), the program reads the x and y components of acceleration (ignoring z), and detects a
- * contact when the magnitude of the 3-period average of the x-y vector exceeds an empirically
- * determined XY_ACCELERATION_THRESHOLD.  On contact detection, the forward speed is increased to
- * FULL_SPEED from the default SEARCH_SPEED, simulating a "fight or flight" response.
- *
- * The program attempts to detect contact only when the Zumo is going straight.  When it is
- * executing a turn at the sumo ring border, the turn itself generates an acceleration in the x-y
- * plane, so the acceleration reading at that time is difficult to interpret for contact detection.
- * Since the Zumo also accelerates forward out of a turn, the acceleration readings are also ignored
- * for MIN_DELAY_AFTER_TURN milliseconds after completing a turn. To further avoid false positives,
- * a MIN_DELAY_BETWEEN_CONTACTS is also specified.
- *
- * This example also contains the following enhancements:
- *
- * - uses the buzzer to play a sound effect ("charge" melody) at start of competition and whenever
- *   contact is made with an opposing robot
- *
- * - randomizes the turn angle on border detection, so that the Zumo executes a more effective
- *   search pattern
- *
- * - supports a FULL_SPEED_DURATION_LIMIT, allowing the robot to switch to a SUSTAINED_SPEED after
- *   a short period of forward movement at FULL_SPEED.  In the example, both speeds are set to 400
- *   (max), but this feature may be useful to prevent runoffs at the turns if the sumo ring surface
- *   is unusually smooth.
- *
- * - logging of accelerometer output to the serial monitor when LOG_SERIAL is #defined.
- *
- * This example also makes use of the public domain RunningAverage library from the Arduino website;
- * the relevant code has been copied into this .ino file and does not need to be downloaded
- * separately.
- */
-#include <Arduino.h>
-#include <avr/pgmspace.h>
-#include <Wire.h>
-#include <Zumo32U4.h>
-
-// #define LOG_SERIAL // write log output to serial port
-
-// Change next line to this if you are using the older Zumo 32U4
-// with a black and green LCD display:
-// Zumo32U4LCD display;
-Zumo32U4OLED display;
-
-Zumo32U4ButtonA button;
-
-// Accelerometer Settings
-#define RA_SIZE 3  // number of readings to include in running average of accelerometer readings
-#define XY_ACCELERATION_THRESHOLD 2400  // for detection of contact (~16000 = magnitude of acceleration due to gravity)
-
-// Reflectance Sensor Settings
-#define NUM_SENSORS 5
-unsigned int sensor_values[NUM_SENSORS];
-// this might need to be tuned for different lighting conditions, surfaces, etc.
-#define QTR_THRESHOLD  1000 // microseconds
-Zumo32U4LineSensors sensors;
-
-// Motor Settings
-Zumo32U4Motors motors;
-
-// these might need to be tuned for different motor types
-#define REVERSE_SPEED     200 // 0 is stopped, 400 is full speed
-#define TURN_SPEED        200
-#define SEARCH_SPEED      200
-#define SUSTAINED_SPEED   400 // switches to SUSTAINED_SPEED from FULL_SPEED after FULL_SPEED_DURATION_LIMIT ms
-#define FULL_SPEED        400
-#define STOP_DURATION     100 // ms
-#define REVERSE_DURATION  200 // ms
-#define TURN_DURATION     300 // ms
-
-#define RIGHT 1
-#define LEFT -1
-
-enum ForwardSpeed { SearchSpeed, SustainedSpeed, FullSpeed };
-ForwardSpeed _forwardSpeed;  // current forward speed setting
-unsigned long full_speed_start_time;
-#define FULL_SPEED_DURATION_LIMIT     250  // ms
-
-// Sound Effects
-Zumo32U4Buzzer buzzer;
-const char sound_effect[] PROGMEM = "O4 T100 V15 L4 MS g12>c12>e12>G6>E12 ML>G2"; // "charge" melody
- // use V0 to suppress sound effect; v15 for max volume
-
- // Timing
-unsigned long loop_start_time;
-unsigned long last_turn_time;
-unsigned long contact_made_time;
-#define MIN_DELAY_AFTER_TURN          400  // ms = min delay before detecting contact event
-#define MIN_DELAY_BETWEEN_CONTACTS   1000  // ms = min delay between detecting new contact event
-
-// RunningAverage class
-// based on RunningAverage library for Arduino
-// source:  http://playground.arduino.cc/Main/RunningAverage
-template <typename T>
-class RunningAverage
-{
-  public:
-    RunningAverage(void);
-    RunningAverage(int);
-    ~RunningAverage();
-    void clear();
-    void addValue(T);
-    T getAverage() const;
-    void fillValue(T, int);
-  protected:
-    int _size;
-    int _cnt;
-    int _idx;
-    T _sum;
-    T * _ar;
-    static T zero;
-};
-
-// Accelerometer Class -- extends the Zumo32U4IMU class to support reading and
-//   averaging the x-y acceleration vectors from the accelerometer
-class Accelerometer : public Zumo32U4IMU
-{
-  typedef struct acc_data_xy
-  {
-    unsigned long timestamp;
-    int x;
-    int y;
-    float dir;
-  } acc_data_xy;
-
-  public:
-    Accelerometer() : ra_x(RA_SIZE), ra_y(RA_SIZE) {};
-    ~Accelerometer() {};
-    void getLogHeader(void);
-    void readAcceleration(unsigned long timestamp);
-    float len_xy() const;
-    float dir_xy() const;
-    int x_avg(void) const;
-    int y_avg(void) const;
-    long ss_xy_avg(void) const;
-    float dir_xy_avg(void) const;
-  private:
-    acc_data_xy last;
-    RunningAverage<int> ra_x;
-    RunningAverage<int> ra_y;
-};
+#include <Sumo.h>
 
 Accelerometer acc;
 boolean in_contact;  // set when accelerometer detects contact with opposing robot
 
+Zumo32U4LCD display;
+Zumo32U4ButtonA button;
+Zumo32U4LineSensors sensors;
+// Motor Settings
+Zumo32U4Motors motors;
+// Sound Effects
+Zumo32U4Buzzer buzzer;
+
 // forward declaration
 void setForwardSpeed(ForwardSpeed speed);
 void waitForButton();
+
+// execute turn
+// direction:  RIGHT or LEFT
+// randomize: to improve searching
+void turn(char direction, bool randomize);
+
+void setForwardSpeed(ForwardSpeed speed);
+
+int getForwardSpeed();
+
+// check for contact, but ignore readings immediately after turning or losing contact
+bool check_for_contact();
+
+// sound horn and accelerate on contact -- fight or flight
+void on_contact_made();
+
+// reset forward speed
+void on_contact_lost();
 
 void setup()
 {
@@ -176,7 +56,7 @@ void setup()
 
   ledYellow(1);
   buzzer.playMode(PLAY_AUTOMATIC);
-  waitForButtonAndCountDown(false);
+  waitForButton();
 }
 
 void waitForButton()
@@ -208,7 +88,7 @@ void loop()
     // if button is pressed, stop and wait for another press to go again
     motors.setSpeeds(0, 0);
     button.waitForRelease();
-    waitForButtonAndCountDown(true);
+    waitForButton();
   }
 
   loop_start_time = millis();
@@ -234,13 +114,17 @@ void loop()
   {
     if (check_for_contact()) on_contact_made();
     int speed = getForwardSpeed();
+  
+    if(millis() - displayTime >= 4000 || displayed != true){
+      display.print(speed);
+      display.print(" RPM");
+      displayed = true;
+      displayTime = millis();
+    }
     motors.setSpeeds(speed, speed);
   }
 }
 
-// execute turn
-// direction:  RIGHT or LEFT
-// randomize: to improve searching
 void turn(char direction, bool randomize)
 {
 #ifdef LOG_SERIAL
@@ -288,7 +172,6 @@ int getForwardSpeed()
   return speed;
 }
 
-// check for contact, but ignore readings immediately after turning or losing contact
 bool check_for_contact()
 {
   static long threshold_squared = (long) XY_ACCELERATION_THRESHOLD * (long) XY_ACCELERATION_THRESHOLD;
@@ -297,7 +180,6 @@ bool check_for_contact()
     (loop_start_time - contact_made_time > MIN_DELAY_BETWEEN_CONTACTS);
 }
 
-// sound horn and accelerate on contact -- fight or flight
 void on_contact_made()
 {
 #ifdef LOG_SERIAL
@@ -309,9 +191,11 @@ void on_contact_made()
   setForwardSpeed(FullSpeed);
   buzzer.playFromProgramSpace(sound_effect);
   ledRed(1);
+  display.setCursor(0,1) ; //sets cursor to second line first row
+  display.print(F("Contact!"));
+  displayTime+= 4000;
 }
 
-// reset forward speed
 void on_contact_lost()
 {
 #ifdef LOG_SERIAL
@@ -323,140 +207,5 @@ void on_contact_lost()
   ledRed(0);
 }
 
-// class Accelerometer -- member function definitions
-
-void Accelerometer::getLogHeader(void)
-{
-  Serial.print("millis    x      y     len     dir  | len_avg  dir_avg  |  avg_len");
-  Serial.println();
-}
-
-void Accelerometer::readAcceleration(unsigned long timestamp)
-{
-  readAcc();
-  if (a.x == last.x && a.y == last.y) return;
-
-  last.timestamp = timestamp;
-  last.x = a.x;
-  last.y = a.y;
-
-  ra_x.addValue(last.x);
-  ra_y.addValue(last.y);
-
-#ifdef LOG_SERIAL
- Serial.print(last.timestamp);
- Serial.print("  ");
- Serial.print(last.x);
- Serial.print("  ");
- Serial.print(last.y);
- Serial.print("  ");
- Serial.print(len_xy());
- Serial.print("  ");
- Serial.print(dir_xy());
- Serial.print("  |  ");
- Serial.print(sqrt(static_cast<float>(ss_xy_avg())));
- Serial.print("  ");
- Serial.print(dir_xy_avg());
- Serial.println();
-#endif
-}
-
-float Accelerometer::len_xy() const
-{
-  return sqrt(last.x*a.x + last.y*a.y);
-}
-
-float Accelerometer::dir_xy() const
-{
-  return atan2(last.x, last.y) * 180.0 / M_PI;
-}
-
-int Accelerometer::x_avg(void) const
-{
-  return ra_x.getAverage();
-}
-
-int Accelerometer::y_avg(void) const
-{
-  return ra_y.getAverage();
-}
-
-long Accelerometer::ss_xy_avg(void) const
-{
-  long x_avg_long = static_cast<long>(x_avg());
-  long y_avg_long = static_cast<long>(y_avg());
-  return x_avg_long*x_avg_long + y_avg_long*y_avg_long;
-}
-
-float Accelerometer::dir_xy_avg(void) const
-{
-  return atan2(static_cast<float>(x_avg()), static_cast<float>(y_avg())) * 180.0 / M_PI;
-}
 
 
-
-// RunningAverage class
-// based on RunningAverage library for Arduino
-// source:  http://playground.arduino.cc/Main/RunningAverage
-// author:  Rob.Tillart@gmail.com
-// Released to the public domain
-
-template <typename T>
-T RunningAverage<T>::zero = static_cast<T>(0);
-
-template <typename T>
-RunningAverage<T>::RunningAverage(int n)
-{
-  _size = n;
-  _ar = (T*) malloc(_size * sizeof(T));
-  clear();
-}
-
-template <typename T>
-RunningAverage<T>::~RunningAverage()
-{
-  free(_ar);
-}
-
-// resets all counters
-template <typename T>
-void RunningAverage<T>::clear()
-{
-  _cnt = 0;
-  _idx = 0;
-  _sum = zero;
-  for (int i = 0; i< _size; i++) _ar[i] = zero;  // needed to keep addValue simple
-}
-
-// adds a new value to the data-set
-template <typename T>
-void RunningAverage<T>::addValue(T f)
-{
-  _sum -= _ar[_idx];
-  _ar[_idx] = f;
-  _sum += _ar[_idx];
-  _idx++;
-  if (_idx == _size) _idx = 0;  // faster than %
-  if (_cnt < _size) _cnt++;
-}
-
-// returns the average of the data-set added so far
-template <typename T>
-T RunningAverage<T>::getAverage() const
-{
-  if (_cnt == 0) return zero; // NaN ?  math.h
-  return _sum / _cnt;
-}
-
-// fill the average with a value
-// the param number determines how often value is added (weight)
-// number should preferably be between 1 and size
-template <typename T>
-void RunningAverage<T>::fillValue(T value, int number)
-{
-  clear();
-  for (int i = 0; i < number; i++)
-  {
-    addValue(value);
-  }
-}
